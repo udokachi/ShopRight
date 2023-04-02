b09900cc211c820bd6cfe76c8cdc8f0645686c72
udokachi <chidinma242@gmail.com>
2023-04-02 03:10:51 +0100

    updated the port for the server

--- Customer/index.js
+++ Customer/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 
+import dotenv from "dotenv";
+dotenv.config()
 const app = express();
+
+const Port = process.env.PORT || 8001
 
 app.use(express.json());
 
@@ -8,6 +12,6 @@ app.use('/',(req, res, next) => {
     return res.status(200).json({"message":"Hello from customer"})
 })
 
-app.listen(8001, () => {
+app.listen(Port, () => {
     console.log("Customer is reading on Port 8001")
 })
